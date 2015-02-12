e8dd634900938677e5b0b1710c23ba371e55b6e7
michal_krzeminski <michkrzeminski@gmail.com>
2015-02-12 06:54:22 +0100

    zmiany do edycji ustawień;

--- src/main/java/pl/atmoterm/atmorozliczenia/MainWindowController.java
+++ src/main/java/pl/atmoterm/atmorozliczenia/MainWindowController.java
@@ -1,30 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.atmoterm.atmorozliczenia;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author hrdlo_000
- */
 public class MainWindowController implements Initializable {
 
+    private static final Logger logger = Logger.getLogger(MainWindowController.class.getName());
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    }
+
     @FXML
     private void handleSettings(ActionEvent event) {
-        
+        try {
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Ustawienia");
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/settings/SettingsPanel.fxml"));
+            Scene scene = new Scene(root);
+            newWindow.setScene(scene);
+            newWindow.showAndWait();
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/main/java/pl/atmoterm/atmorozliczenia/settings/services/SettingsManager.java
+++ src/main/java/pl/atmoterm/atmorozliczenia/settings/services/SettingsManager.java
@@ -1,5 +1,6 @@
 package pl.atmoterm.atmorozliczenia.settings.services;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Properties;
@@ -7,54 +8,66 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SettingsManager {
-    
+
     private static final Logger logger = Logger.getLogger(SettingsManager.class.getName());
-    
+
     private static SettingsManager manager;
+
     public static SettingsManager get() {
-        if(manager == null) {
+        if (manager == null) {
             manager = new SettingsManager();
         }
         return manager;
     }
-    
+
     private final Properties properties;
     private boolean initialized = false;
-    
+
     private SettingsManager() {
         properties = new Properties();
         try (FileInputStream input = new FileInputStream("config.properties")) {
             properties.load(input);
             initialized = true;
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             logger.log(Level.SEVERE, "Błąd podczas otwierania ustawień", ex);
             initialized = false;
         }
     }
-    
+
     public void save() {
-        try (FileOutputStream output = new FileOutputStream("config.properties")) {
-            properties.store(output, "Ustawienia dla AtmoRozliczenia");
-            initialized = true;
-        } catch(Exception ex) {
-            logger.log(Level.SEVERE, "Błąd podczas zapisywania ustawień", ex);
+        try {
+            File fo = new File("config.properties");
+            if (!fo.exists()) {
+                fo.createNewFile();
+            }
+            try (FileOutputStream output = new FileOutputStream(fo)) {
+                properties.store(output, "Ustawienia dla AtmoRozliczenia");
+                initialized = true;
+            } catch (Exception ex) {
+                logger.log(Level.SEVERE, "Błąd podczas zapisywania ustawień", ex);
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Błąd podczas zapisywania ustawień", e);
         }
     }
-    
+
     public String getGoogleClientId() {
         return properties.getProperty("google.clientId");
     }
-    
+
     public void setGoogleClientId(String clientId) {
         properties.setProperty("google.clientId", clientId);
     }
-    
+
     public String getGoogleClientSecret() {
         return properties.getProperty("google.clientSecret");
     }
-    
+
     public void setGoogleClientSecret(String clientSecret) {
         properties.setProperty("google.clientSecret", clientSecret);
     }
-}
 
+    public boolean isInitialized() {
+        return initialized;
+    }
+}
